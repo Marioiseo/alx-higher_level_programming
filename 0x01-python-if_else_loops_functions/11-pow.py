5cca1c44d3042cb1a725d57e0a0b1f57ae828716
Mariohany <mariohany302@gmail.com>
2023-08-08 08:54:49 -0700

    lololol

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,9 +1,3 @@
 #!/usr/bin/python3
 def pow(a, b):
-    c = a 
-    for i in range(0, b - 1):
-        if b > 0:
-            c = c * a
-        if b < 0:
-            c = c / a
-    return c
+    return a ** b
