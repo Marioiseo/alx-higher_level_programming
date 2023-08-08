e9a100f08b8b92d86ede37c4ba77aa9d797d9b5f
Mariohany <mariohany302@gmail.com>
2023-08-08 02:07:52 -0700

    lovely

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,8 +5,8 @@ nummod = abs(number) % 10
 if number < 0:
     nummod = nummod * -1
 if nummod == 0:
-    print(f"last digit of {number} is {nummod} and is 0")
+    print(f"Last digit of {number} is {nummod} and is 0")
 elif nummod > 5:
-    print(f"last digit of {number} is {nummod} and is greater than 5")
+    print(f"Last digit of {number} is {nummod} and is greater than 5")
 else:
-    print(f"last digit of {number} is {nummod} and is less than 6 and not 0")
+    print(f"Last digit of {number} is {nummod} and is less than 6 and not 0")
