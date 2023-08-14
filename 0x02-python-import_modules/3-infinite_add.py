b5ccad03a80f96ebb355a871ebd7335c89f295b4
Mariohany <mariohany302@gmail.com>
2023-08-14 02:44:25 -0700

    lovely

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 
-if __name__== "__main__":
+if __name__ == "__main__":
     import sys
     argv = sys.argv[1:]
     arglen = len(argv)
-    num = int(argv[0])
-    for i in range(1, arglen):
-        num += int(argv[i])
-    print(num)
+    if arglen == 0:
+        print(0)
+    else:
+        num = int(argv[0])
+        for i in range(1, arglen):
+            num += int(argv[i])
+        print(num)
