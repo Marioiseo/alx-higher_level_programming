69002237863326eb0df144eeb96156b29f58a5f0
Mariohany <mariohany302@gmail.com>
2023-08-14 12:15:45 -0700

    silly

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
     for i in range(0, len(my_list)):
-        n = int(my_list[i])
-        print(f"{n}")
+        print("{:d}".format(my_list[i]))
